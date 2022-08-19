1b8d5371954bda5de32f688e13e35d0468c03dca
roanvonb <roanvonb@gmail.com>
2022-08-19 22:49:26 +1000

    added get route to fix express routing issue

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 require("./routes")(app);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
