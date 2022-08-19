5bed4bf878f79000b2b992be104bb53d11064616
roanvonb <roanvonb@gmail.com>
2022-08-19 22:12:15 +1000

    configured server to serve react build folder

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require("express"); // import express module (simplifies routing/requests, among other things)
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 require("./routes")(app);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
