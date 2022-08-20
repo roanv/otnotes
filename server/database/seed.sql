b513b271945581fba16850d69ba42085be406b54
roanvonb <roanvonb@gmail.com>
2022-08-20 14:16:21 +1000

    moved database schema and seed values to separate file

---
+++ server/database/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO goal (name) VALUES ('DATABASE TEST');
+INSERT INTO goal (name) VALUES ('Balance');
+INSERT INTO goal (name) VALUES ('Ocular Control');
+INSERT INTO goal (name) VALUES ('Bilateral Integration');
+INSERT INTO goal (name) VALUES ('Fine Motor Control');
+INSERT INTO goal (name) VALUES ('Visual Perception');
+
+INSERT INTO goal (name) VALUES ('DATABSE TEST');
+INSERT INTO goal (name) VALUES ('Weight shift');
+INSERT INTO goal (name) VALUES ('Elongation');
+INSERT INTO goal (name) VALUES ('Rotation');
+INSERT INTO goal (name) VALUES ('Visual Perception');
+INSERT INTO goal (name) VALUES ('Narrow base of support');
+INSERT INTO goal (name) VALUES ('Elevate point of gravity');
+INSERT INTO goal (name) VALUES ('Decrease external support');
+INSERT INTO goal (name) VALUES ('Increase external resistance');
+INSERT INTO goal (name) VALUES ('Open & closed eyes');
--- server/routes/goals.js
+++ server/routes/goals.js
@@ -7,12 +7,4 @@ router.get("/", async (req, res) => {
   res.json(rows.map((goal) => goal.name));
 });
 
-// [
-//   "Balance",
-//   "Ocular Control",
-//   "Bilateral Integration",
-//   "Fine Motor Control",
-//   "Visual Perception",
-// ];
-
 module.exports = router;
