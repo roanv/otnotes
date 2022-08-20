ac8afa935ea69df78962268a9f73d6042ca506db
roanvonb <roanvonb@gmail.com>
2022-08-20 14:07:36 +1000

    goals now queried from pg databse

--- server/routes/goals.js
+++ server/routes/goals.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database/pg");
 
 router.get("/", async (req, res) => {
-  res.json([
-    "Balance",
-    "Ocular Control",
-    "Bilateral Integration",
-    "Fine Motor Control",
-    "Visual Perception",
-  ]);
+  const { rows } = await db.query("SELECT * FROM goal");
+  res.json(rows.map((goal) => goal.name));
 });
+
+// [
+//   "Balance",
+//   "Ocular Control",
+//   "Bilateral Integration",
+//   "Fine Motor Control",
+//   "Visual Perception",
+// ];
 
 module.exports = router;
