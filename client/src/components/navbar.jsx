01fbe0f94dc9f890fc1634675152741e38098b4e
roanvonb <roanvonb@gmail.com>
2022-08-22 14:48:10 +1000

    added menu icon to navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,30 +1,33 @@
-import {
-  AppBar,
-  Toolbar,
-  Button,
-  Box,
-  IconButton,
-  Container,
-} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+
+import { AppBar, Toolbar, Button, IconButton, Container } from "@mui/material";
+
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavBar = ({ pages, button }) => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                component={RouterLink}
-                to={page}
-                key={page}
-                sx={{ my: 2, color: "inherit" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {pages.map((page) => (
+            <Button
+              component={RouterLink}
+              to={page}
+              key={page}
+              sx={{ my: 2, color: "inherit" }}
+            >
+              {page}
+            </Button>
+          ))}
           {button}
         </Toolbar>
       </Container>
